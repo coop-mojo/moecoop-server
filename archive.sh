51da119b4ee93dbbaff7eb6f4108208d0a2e0873
もじょんぬ <moe.e.coop@gmail.com>
2017-03-23 23:09:51 +0900

    archive.sh を修正

--- archive.sh
+++ archive.sh
@@ -4,5 +4,5 @@ dub upgrade
 sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
 dub build -c server -b release
 strip fukurod
-tar cvzf moecoop.tgz fukurod LICENSE README.md resource docs
+tar cvzf moecoop.tgz fukurod LICENSE README.md resource
 rm fukurod
