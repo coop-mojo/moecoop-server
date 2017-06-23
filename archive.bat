f1df1521af1083161bc34435cdfbd851c43d8459
もじょんぬ <moe.e.coop@gmail.com>
2017-06-24 04:36:45 +0900

    [AppVeyor] パッケージにデータファイルを含めるようにした

--- archive.bat
+++ archive.bat
@@ -3,7 +3,7 @@
 dub upgrade
 dub build -a %1 -b %2
 
-powershell -Command Compress-Archive -Path fukurod.exe, LICENSE, README.md, libeay32.dll, ssleay32.dll -DestinationPath moecoop-server-%1.zip
+powershell -Command Compress-Archive -Path fukurod.exe, LICENSE, README.md, libeay32.dll, ssleay32.dll, resource -DestinationPath moecoop-server-%1.zip
 
 if %1==x86 (
     powershell -Command Compress-Archive -Path libevent.dll -DestinationPath moecoop-server-%1.zip -Update
