b128c04c7c448d846c1f624e062aaa951d44a6f5
もじょんぬ <moe.e.coop@gmail.com>
2017-07-10 22:15:00 +0900

    WIP

--- archive.sh
+++ archive.sh
@@ -3,7 +3,4 @@
 docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub upgrade
 sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
 docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub build -b release
-docker run --rm -it -v $PWD:/src moecoop/docker-fukuro chown -R $USER:$USER fukurod resource
-strip fukurod
 tar cvzf moecoop.tgz fukurod LICENSE README.md resource
-rm fukurod
