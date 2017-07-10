9cec05ff8d5f89330e077becadfdf1f4fd301757
もじょんぬ <moe.e.coop@gmail.com>
2017-07-10 21:51:28 +0900

    生成されるファイルの権限がおかしかったのを修正

--- archive.sh
+++ archive.sh
@@ -3,7 +3,7 @@
 docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub upgrade
 sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
 docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub build -b release
-ls -l
+docker run --rm -it -v $PWD:/src moecoop/docker-fukuro chown -R $USER:$USER fukurod resource
 strip fukurod
 tar cvzf moecoop.tgz fukurod LICENSE README.md resource
 rm fukurod
