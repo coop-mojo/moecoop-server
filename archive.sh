efebda75d0c87fafa7d950e5a1e15c13023e26b4
もじょんぬ <moe.e.coop@gmail.com>
2017-07-10 21:21:38 +0900

    イメージ名が間違っていたのを修正

--- archive.sh
+++ archive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run --rm -it -v $PWD:/src docker-fukuro dub upgrade
+docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub upgrade
 sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
-docker run --rm -it -v $PWD:/src docker-fukuro dub build -b release
+docker run --rm -it -v $PWD:/src moecoop/docker-fukuro dub build -b release
 strip fukurod
 tar cvzf moecoop.tgz fukurod LICENSE README.md resource
 rm fukurod
