65e2d84a5115c6ad98c7a472b2756fe25f9c32ca
もじょんぬ <moe.e.coop@gmail.com>
2018-06-11 02:32:53 +0900

    docker login を修正

--- update-heroku.sh
+++ update-heroku.sh
@@ -3,6 +3,6 @@
 token=$1
 image=$2
 
-docker login -u=_ -p="$token" registry.heroku.com;
+echo "$token" | docker login -u=_ --password-stdin registry.heroku.com;
 docker tag $image:latest registry.heroku.com/moecoop-server/web;
 docker push registry.heroku.com/moecoop-server/web;
