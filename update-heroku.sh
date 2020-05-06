aed8f3d3a8069821c789f23b0f022222b70dc995
もじょんぬ <moe.e.coop@gmail.com>
2020-05-06 11:53:35 +0900

    Heroku で push したイメージでリリースしてなかったのを修正

--- update-heroku.sh
+++ update-heroku.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-token=$1
-image=$2
+email=$1
+token=$2
+image=$3
+
+cat > ~/.netrc <<EOF
+machine api.heroku.com
+    login ${email}
+    password ${token}
+EOF
 
 echo "$token" | docker login -u=_ --password-stdin registry.heroku.com;
 docker tag $image:latest registry.heroku.com/moecoop-server/web;
 docker push registry.heroku.com/moecoop-server/web;
+
+heroku container:release web --app moecoop-server;
